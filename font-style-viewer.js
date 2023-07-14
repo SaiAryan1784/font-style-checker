5378893e643d2cb5f8c54830e6a1c44c4adb1eee
Sai Aryan Goswami <111999256+SaiAryan1784@users.noreply.github.com>
2023-07-14 19:45:43 +0530

    Initial commit

---
+++ font-style-viewer.js
@@ -0,0 +1,36 @@
+const Courier = document.getElementById("Courier")
+const Franklin = document.getElementById("Franklin")
+const Gill = document.getElementById("Gill")
+const Verdana = document.getElementById("Verdana")
+const Segoe = document.getElementById("Segoe")
+const Georgia = document.getElementById("Georgia")
+
+function fs1(){
+    let input1 = document.getElementById("input1").value
+    Courier.innerHTML = input1
+}
+
+function fs2(){
+    let input2 = document.getElementById("input2").value
+    Franklin.innerHTML = input2
+}
+
+function fs3(){
+    let input3 = document.getElementById("input3").value
+    Gill.innerHTML = input3
+}
+
+function fs4(){
+    let input4 = document.getElementById("input4").value
+    Verdana.innerHTML = input4
+}
+
+function fs5(){
+    let input5 = document.getElementById("input5").value
+    Segoe.innerHTML = input5
+}
+
+function fs6(){
+    let input6 = document.getElementById("input6").value
+    Georgia.innerHTML = input6
+}
